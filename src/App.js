999508c22a922db4a0122275c5e9ea8899843f23
Himanshusaini12 <107533833+Himanshusaini12@users.noreply.github.com>
2023-03-17 13:58:03 +0530

    bomb

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
-import Body from './components/body';
+import PostNumber from './components/body';
 function App() {
   return (
     <div className="App">
     
-    <Header/>
+    
     <Body/>
     
     </div>
